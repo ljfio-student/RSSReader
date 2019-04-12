f1c3d166a4d9ce75e4d9cc4b600b1cd10c3d7ba5
Luke Fisher <mail@lukefisher.me>
2019-04-12 22:01:13 +0100

    Order by latest data first

--- api/content.php
+++ api/content.php
@@ -26,7 +26,8 @@ $session = $user_statement->fetch(PDO::FETCH_ASSOC);
 $statement = $database->prepare("SELECT `content`.`id`, `content`.`feed_id`, `content`.`title`, `content`.`description`, `content`.`link`, `content`.`date`
 FROM `content`
 INNER JOIN `user_feed` ON `user_feed`.`feed_id` = `content`.`feed_id`
-WHERE `user_feed`.`user_id` = :user_id");
+WHERE `user_feed`.`user_id` = :user_id
+ORDER BY `content`.`date` DESC");
 $statement->execute([":user_id" => $session["user_id"]]);
 
 $articles = [];
